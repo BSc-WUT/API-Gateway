553cd0f220055d8499a087657470556354a49593
Bartolo72 <bartosz.kosinski.b@gmail.com>
2023-12-25 02:18:21 +0100

    initial commit

---
+++ src/main.py
@@ -0,0 +1,107 @@
+from fastapi import FastAPI, UploadFile, HTTPException
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+from typing import List
+import requests
+import os
+import json
+
+from .models import Model, NetworkFlow, NetworkFlowFull
+
+
+
+def get_env_vars() -> dict:
+    load_dotenv()
+    return {
+        "DB_API_URL": f'http://{os.getenv("DB_API")}:{os.getenv("DB_API_PORT")}',
+        "ML_API_URL": f'http://{os.getenv("ML_API")}:{os.getenv("ML_API_PORT")}',
+        "API_PORT": os.getenv('API_PORT'),
+    }
+
+
+app = FastAPI()
+ENV_VARS = get_env_vars()
+
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+""" ML API """
+
+
+@app.get('/models')
+def get_models() -> JSONResponse:
+    response: requests.Response = requests.get(f"{ENV_VARS['ML_API_URL']}/models")
+    result: dict = response.json()
+    models: List[Model] = result
+    return models
+
+
+@app.get('/models/{model_name}')
+def get_model(model_name: str) -> JSONResponse:
+    response: requests.Response = requests.get(f"{ENV_VARS['ML_API_URL']}/models/{model_name}")
+    result: dict = response.json()
+    models: Model = result
+    return models
+
+
+@app.get('/models/{model_name}/activate')
+def activate_model(model_name: str) -> JSONResponse:
+    response: requests.Response = requests.get(f"{ENV_VARS['ML_API_URL']}/models/activate/{model_name}")
+    result: dict = response.json()
+    return result
+
+
+@app.get('/models/{model_name}/deactivate')
+def deactivate_model(model_name: str) -> JSONResponse:
+    response: requests.Response = requests.get(f"{ENV_VARS['ML_API_URL']}/models/deactivate/{model_name}")
+    result: dict = response.json()
+    return result
+
+
+@app.post('/models/{model_name}/predict')
+def predict(model_name: str, flow: NetworkFlow) -> JSONResponse:
+    response: requests.Response = requests.post(f"{ENV_VARS['ML_API_URL']}/models/predict/{model_name}", data=json.dumps(flow.dict()))
+    result: dict = response.json()
+    return result
+
+""" DB_API """
+'''
+1. Get flows
+2. Get flow
+3. Update flow
+'''
+
+@app.get('/network_flows')
+def get_flows() -> List[NetworkFlow]:
+    response: requests.Response = requests.get(f"{ENV_VARS['DB_API_URL']}/network_flows")
+    flows: List[NetworkFlow] = [hit.get('_source', {}) for hit in response.json().get('hits', {}).get('hits', [])]
+    return flows
+
+
+@app.get('/network_flows/{flow_id}')
+def get_flow(flow_id: str) -> JSONResponse:
+    response: requests.Response = requests.get(f"{ENV_VARS['DB_API_URL']}/network_flows/{flow_id}")
+    try:
+        flow: NetworkFlow = response.json()
+    except Exception:
+        flow: dict = {}
+    return flow
+
+
+@app.put('/network_flows/{flow_id}')
+def update_flow(flow_id: str, flow: NetworkFlowFull) -> JSONResponse:
+    response: requests.Response = requests.put(f"{ENV_VARS['DB_API_URL']}/network_flows/{flow_id}", data=json.dumps(flow.dict()))
+    result: dict = response.json()
+    return result
---
+++ src/models.py
@@ -0,0 +1,120 @@
+from pydantic import BaseModel
+from typing import List
+
+
+class ModelLayer(BaseModel):
+    layer_name: str
+    input_shape: str
+    output_shape: str
+    params: int
+    kernel_size: str
+
+
+class Model(BaseModel):
+    is_active: bool
+    name: str
+    layers: List[ModelLayer]
+    total_params: int
+    trainable_params: int
+    non_trainable_params: int
+    total_mult_adds: float
+    input_size_MB: float
+    forward_backward_pass_size_MB: float
+    params_size_MB: float
+    estimated_total_size_MB: float
+
+
+class ModelShort(BaseModel):
+    layers: List[str]
+    input_shape: str
+    output_shape: str
+    total_params: int
+
+
+class NetworkFlow(BaseModel):
+    protocol: float
+    flow_duration: float
+    flow_byts_s: float
+    flow_pkts_s: float
+    fwd_pkts_s: float
+    bwd_pkts_s: float
+    tot_fwd_pkts: float
+    tot_bwd_pkts: float
+    totlen_fwd_pkts: float
+    totlen_bwd_pkts: float
+    fwd_pkt_len_max: float
+    fwd_pkt_len_min: float
+    fwd_pkt_len_mean: float
+    fwd_pkt_len_std: float
+    bwd_pkt_len_max: float
+    bwd_pkt_len_min: float
+    bwd_pkt_len_mean: float
+    bwd_pkt_len_std: float
+    pkt_len_max: float
+    pkt_len_min: float
+    pkt_len_mean: float
+    pkt_len_std: float
+    pkt_len_var: float
+    fwd_header_len: float
+    bwd_header_len: float
+    fwd_seg_size_min: float
+    fwd_act_data_pkts: float
+    flow_iat_mean: float
+    flow_iat_max: float
+    flow_iat_min: float
+    flow_iat_std: float
+    fwd_iat_tot: float
+    fwd_iat_max: float
+    fwd_iat_min: float
+    fwd_iat_mean: float
+    fwd_iat_std: float
+    bwd_iat_tot: float
+    bwd_iat_max: float
+    bwd_iat_min: float
+    bwd_iat_mean: float
+    bwd_iat_std: float
+    fwd_psh_flags: float
+    bwd_psh_flags: float
+    fwd_urg_flags: float
+    bwd_urg_flags: float
+    fin_flag_cnt: float
+    syn_flag_cnt: float
+    rst_flag_cnt: float
+    psh_flag_cnt: float
+    ack_flag_cnt: float
+    urg_flag_cnt: float
+    ece_flag_cnt: float
+    down_up_ratio: float
+    pkt_size_avg: float
+    init_fwd_win_byts: float
+    init_bwd_win_byts: float
+    active_max: float
+    active_min: float
+    active_mean: float
+    active_std: float
+    idle_max: float
+    idle_min: float
+    idle_mean: float
+    idle_std: float
+    fwd_byts_b_avg: float
+    fwd_pkts_b_avg: float
+    bwd_byts_b_avg: float
+    bwd_pkts_b_avg: float
+    fwd_blk_rate_avg: float
+    bwd_blk_rate_avg: float
+    fwd_seg_size_avg: float
+    bwd_seg_size_avg: float
+    cwe_flag_count: float
+    subflow_fwd_pkts: float
+    subflow_bwd_pkts: float
+    subflow_fwd_byts: float
+    subflow_bwd_byts: float
+
+
+class NetworkFlowFull(NetworkFlow):
+    label: str
+    src_ip: str
+    dst_ip: str
+    src_port: str
+    dst_port: str
+    timestamp: str
